124c99f0f54a1af4c49177294f01e2257e58f57d
Lebeil <liedel75@gmail.com>
2020-04-17 19:40:55 +0200

    maj dark-trader

--- lib/dark_trader.rb
+++ lib/dark_trader.rb
@@ -52,3 +52,33 @@ crypto_price_array = prices
 clean_price(crypto_price_array)
 puts final_array(crypto_name_array,crypto_price_array)
 
+
+# ---------------- autre méthode -------------------#
+
+# def dark_trader
+#     page = Nokogiri::HTML(open('https://coinmarketcap.com/all/views/all'))
+#     # page.title
+
+#     coins = []
+#     prices = [] 
+
+#     begin
+#         page.xpath('//*[@id="__next"]/div/div[2]/div[1]/div[2]/div/div[2]/div[3]/div/table/tbody/tr/td[3]').each do |tr|
+#             coins << tr.text
+#         end
+#     rescue => e
+#         puts "Oups petite erreur, mais c'est pas grave"
+#     end
+
+#     begin
+#         page.xpath('//*[@id="__next"]/div/div[2]/div[1]/div[2]/div/div[2]/div[3]/div/table/tbody/tr/td[5]').each do |tr|
+#             prices << tr.text
+#         end
+#     rescue => e
+#         puts "Oups petite erreur, mais c'est pas grave"
+#     end
+    
+#     puts a = Hash[coins.zip(prices)]
+# end
+
+# dark_trader
